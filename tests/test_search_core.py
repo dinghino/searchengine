9874dab9b756d36d479b2589e15d67940edeff24
Daniele Ding <dinghino@gmail.com>
2017-06-06 12:12:47 +0200

    refactor Item into Phrase w/ small fixes
    fixed some minor issues with the helper class
    refactored name from Item to Phrase
    remove tokenization on Phrase words (duh!)
    refactor tests with actual results.

--- tests/test_search_core.py
+++ tests/test_search_core.py
@@ -2,17 +2,17 @@
 Testing module for the search core functionalities
 """
 from search import core
-from tests.helpers import Item
+from tests.helpers import Phrase
 
 
 class TestCore:
     @classmethod
     def setup_class(cls):
-        cls.items = Item.setup()
+        Phrase.setup()
         cls.search = core.SearchEngine(['words'], limit=10)
 
     def test_single_words(self):
-        seq = Item.get_by_length(max_length=1)
+        seq = Phrase.get_by_length(max_length=1)
 
         res = self.search('inconvene', seq)
         assert res == ['inconvenience']
@@ -21,41 +21,39 @@ class TestCore:
         assert res == ['laughter']
 
     def test_short_phrases(self):
-        seq = Item.get_by_length(3, 5)
+        seq = Phrase.get_by_length(3, 7)
         results = self.search('sherlock holmes', seq)
         reversed_query_results = self.search('holmes sherlock', seq)
-        expected = [
-            'sherlock holmes clapped upon knee',
-            'sherlock holmes foretold exquisite mouth',
-            'sherlock holmes upon grey walls',
-            'sherlock holmes wrote english capital',
-        ]
+
+        expected = ['Sherlock Holmes upon the grey walls.', ]
 
         assert results == expected
         assert reversed_query_results == expected
         results = self.search('watson', seq)
-        assert results == [
-            "watson have learned something",
-            "what wanted what doctor watson",
-        ]
+        assert results == ['Ah, Watson, you have learned something!', ]
 
     def test_search_with_params(self):
-        seq = Item.get_by_length(3, 5)
+        seq = Phrase.get_by_length(7, 10)
         res = self.search('sherlock holmes', seq, limit=2, threshold=.95)
         expected = [
-            'sherlock holmes clapped upon knee',
-            'sherlock holmes foretold exquisite mouth',
+            'About nine o\'clock Sherlock Holmes had not yet returned.',
+            'Sherlock Holmes and I wrote to the old English capital.'
         ]
         assert res == expected
 
     def test_search_long_phrases(self):
-        seq = Item.get_by_length(12, 15)
+        seq = Phrase.get_by_length(7, 20)
         results = self.search('sherlock holmes', seq)
         expected = [
-            'facts these about four clock when sherlock holmes took step between wharf ruffian pursued',            # noqa: E501
-            'moment intimacy there were other traces sherlock holmes stopped front aberdeen shipping company',      # noqa: E501
-            'rushed down just have taken mind understand from some small dealings with sherlock holmes',            # noqa: E501
-            'sherlock holmes brought gush hope which sank into desultory chat with over despair',                   # noqa: E501
-            'sherlock holmes great many scattered papers have whatever bears upon stone professional work attend'   # noqa: E501
+            'About nine o\'clock Sherlock Holmes had not yet returned.',                                             # noqa: E501
+            'Sherlock Holmes and I find that I am a widower and never would look at a wayside public-house.',        # noqa: E501
+            'Sherlock Holmes and I wrote to the old English capital.',                                               # noqa: E501
+            'Sherlock Holmes clapped his hands together in a dark silhouette against the wall at the lock.',         # noqa: E501
+            'Sherlock Holmes clapped the hat upon his knee.',                                                        # noqa: E501
+            'Sherlock Holmes closed his eyes travelled round and round the edge.',                                   # noqa: E501
+            'Sherlock Holmes had brought a gush of hope which sank into a desultory chat with me over in despair.',  # noqa: E501
+            'Sherlock Holmes had foretold, and the exquisite mouth.',                                                # noqa: E501
+            'Sherlock Holmes hailed a four-wheeler which was of the stranger.',                                      # noqa: E501
+            'Sherlock Holmes staggered back, white with chagrin and discontent upon his face.',                      # noqa: E501
         ]
         assert results == expected
